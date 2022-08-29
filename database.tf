b185e955cff58109e26f3e734b3c071e9db5e436
Praveengostu <praveek9@in.ibm.com>
2022-08-29 17:12:53 +0530

    Update database.tf

--- database.tf
+++ database.tf
@@ -1,5 +1,10 @@
 
 
-resource "postgresql_database" "my_db" {
-  name              = var.database
+
+
+resource "postgresql_role" "my_role" {
+  name     = "my_role"
+  login    = true
+  password = var.role_password
+  create_database = true
 }
