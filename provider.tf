e8a25526823efa809ebc1c863fd34b2b9fd5e994
Praveengostu <praveek9@in.ibm.com>
2022-08-29 17:01:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,16 @@
+
+
+terraform {
+  required_providers {
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.17.1"
+    }
+  }
+}
+
+
+
 provider "postgresql" {
   host            = var.host
   port            = var.port
